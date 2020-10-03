215c43c7b544f4fcf4047a9ea8e5077c237ea2b1
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-03 19:13:18 +0300

    Add Create Account With Email and Password

--- QutebaApp-Core/Services/Interfaces/IAuthService.cs
+++ QutebaApp-Core/Services/Interfaces/IAuthService.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using QutebaApp_Data.Models;
+using QutebaApp_Data.ViewModels;
+using System.Threading.Tasks;
 
 namespace QutebaApp_Core.Services.Interfaces
 {
     public interface IAuthService
     {
+        Task<User> CreateAccountWithEmailAndPassword(AuthenticateUserVM authenticateUserVM);
     }
 }

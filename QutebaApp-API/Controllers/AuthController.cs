66241e522e7dcb2e88eea4d2ebc7233020a0e8e7
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-03 18:55:42 +0300

    Update Parameter to a View Model
    Changed the parameter of CreateAccountWithEmailAndPassword() from User model to AuthenticateUserVM view model. Also added more properties to createdUser.

--- QutebaApp-API/Controllers/AuthController.cs
+++ QutebaApp-API/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using FirebaseAdmin.Auth;
 using Microsoft.AspNetCore.Mvc;
 using QutebaApp_Data.Models;
+using QutebaApp_Data.ViewModels;
 using System;
 using System.Threading.Tasks;
 
@@ -13,24 +14,30 @@ namespace QutebaApp_API.Controllers
 
         [HttpPost]
         [Route("CreateAccountWithEmailAndPassword")]
-        public async Task<User> CreateAccountWithEmailAndPassword([FromBody] User user)
+        public async Task<User> CreateAccountWithEmailAndPassword([FromBody] AuthenticateUserVM authenticateUserVM)
         {
             try
             {
                 UserRecordArgs userDetails = new UserRecordArgs()
                 {
-                    DisplayName = user.DisplayName,
-                    Email = user.Email,
-                    Password = user.Password,
+                    DisplayName = authenticateUserVM.DisplayName,
+                    Email = authenticateUserVM.Email,
+                    Password = authenticateUserVM.Password,
                 };
 
                 UserRecord userRecord = await FirebaseAuth.DefaultInstance.CreateUserAsync(userDetails);
-                IUserInfo[] userInfo = userRecord.ProviderData;
 
                 User createdUser = new User()
                 {
+                    ID = userRecord.Uid,
                     DisplayName = userRecord.DisplayName,
-                    Email = userRecord.Email
+                    Email = userRecord.Email,
+                    EmailVerified = userRecord.EmailVerified,
+                    PhotoUrl = userRecord.PhotoUrl,
+                    CreatedAccountWith = userRecord.ProviderData[0].ProviderId,
+                    CreationTimestamp = userRecord.UserMetaData.CreationTimestamp,
+                    LastSignInTimestamp = userRecord.UserMetaData.LastSignInTimestamp,
+                    LastRefreshTimestamp = userRecord.UserMetaData.LastRefreshTimestamp
                 };
 
                 return createdUser;

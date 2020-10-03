b146bbb355a7665d3666a5d5b365f9699b73f539
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-03 14:03:04 +0300

    Add a View Model used to Authenticate the User

---
+++ QutebaApp-Data/ViewModels/AuthenticateUserVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QutebaApp_Data.ViewModels
+{
+    public class AuthenticateUserVM
+    {
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}

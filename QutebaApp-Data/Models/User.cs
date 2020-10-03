8e8a19245c0a6cb032fd6729930d423d33376278
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-03 13:58:58 +0300

    Update User Model

--- QutebaApp-Data/Models/User.cs
+++ QutebaApp-Data/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FirebaseAdmin.Auth;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,8 +7,14 @@ namespace QutebaApp_Data.Models
 {
     public class User
     {
+        public string ID { get; set; }
         public string DisplayName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
+        public bool EmailVerified { get; set; }
+        public string PhotoUrl { get; set; }
+        public string CreatedAccountWith { get; set; }
+        public DateTime? CreationTimestamp { get; set; }
+        public DateTime? LastSignInTimestamp { get; set; }
+        public DateTime? LastRefreshTimestamp { get; set; }
     }
 }

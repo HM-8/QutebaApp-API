ea75740a0ca6dc29715aeca94bbf368ec23a1fe7
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-03 19:14:32 +0300

    Implement Create Account With Email and Password

--- QutebaApp-Core/Services/Implementations/AuthService.cs
+++ QutebaApp-Core/Services/Implementations/AuthService.cs
@@ -1,10 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using FirebaseAdmin.Auth;
+using QutebaApp_Core.Services.Interfaces;
+using QutebaApp_Data.Models;
+using QutebaApp_Data.ViewModels;
+using System;
+using System.Threading.Tasks;
 
 namespace QutebaApp_Core.Services.Implementations
 {
-    public class AuthService
+    public class AuthService : IAuthService
     {
+        public async Task<User> CreateAccountWithEmailAndPassword(AuthenticateUserVM authenticateUserVM)
+        {
+            try
+            {
+                UserRecordArgs userDetails = new UserRecordArgs()
+                {
+                    DisplayName = authenticateUserVM.DisplayName,
+                    Email = authenticateUserVM.Email,
+                    Password = authenticateUserVM.Password,
+                };
+
+                UserRecord userRecord = await FirebaseAuth.DefaultInstance.CreateUserAsync(userDetails);
+
+                User createdUser = new User()
+                {
+                    ID = userRecord.Uid,
+                    DisplayName = userRecord.DisplayName,
+                    Email = userRecord.Email,
+                    EmailVerified = userRecord.EmailVerified,
+                    PhotoUrl = userRecord.PhotoUrl,
+                    CreatedAccountWith = userRecord.ProviderData[0].ProviderId,
+                    CreationTimestamp = userRecord.UserMetaData.CreationTimestamp,
+                    LastSignInTimestamp = userRecord.UserMetaData.LastSignInTimestamp,
+                    LastRefreshTimestamp = userRecord.UserMetaData.LastRefreshTimestamp
+                };
+
+                return createdUser;
+            }
+            catch (Exception e) { throw e; }
+        }
     }
 }

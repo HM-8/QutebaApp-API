b9a252f41a5f893fbb7b2e8eb294aeeaaca254ac
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-10 22:42:19 +0300

    Add Role Model

---
+++ QutebaApp-Data/Models/Role.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace QutebaApp_Data.Models
+{
+    public partial class Role
+    {
+        public Role()
+        {
+            Accounts = new HashSet<Account>();
+        }
+
+        public int Id { get; set; }
+        public string RoleName { get; set; }
+
+        public virtual ICollection<Account> Accounts { get; set; }
+    }
+}

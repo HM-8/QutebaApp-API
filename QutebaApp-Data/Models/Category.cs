367cf350410a46c5ba338edb3b4d345398780881
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-10 22:42:35 +0300

    Add Category Model

---
+++ QutebaApp-Data/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QutebaApp_Data.Models
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            Spendings = new HashSet<Spending>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreationTime { get; set; }
+
+        public virtual ICollection<Spending> Spendings { get; set; }
+    }
+}

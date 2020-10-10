800b368f27116c2e793c22e26961aa9c479a4a4d
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-10 22:42:06 +0300

    Add Profile Model

---
+++ QutebaApp-Data/Models/Profile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QutebaApp_Data.Models
+{
+    public partial class Profile
+    {
+        public Profile()
+        {
+            Spendings = new HashSet<Spending>();
+        }
+
+        public string UserUid { get; set; }
+        public string PhotoUrl { get; set; }
+        public double? Salary { get; set; }
+        public DateTime? SalaryCreationTime { get; set; }
+
+        public virtual Account User { get; set; }
+        public virtual ICollection<Spending> Spendings { get; set; }
+    }
+}

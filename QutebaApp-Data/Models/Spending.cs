32756f6e523c372b64e11db7075427207967f56e
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-10 22:42:53 +0300

    Add Spending Model

---
+++ QutebaApp-Data/Models/Spending.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QutebaApp_Data.Models
+{
+    public partial class Spending
+    {
+        public int Id { get; set; }
+        public string UserUid { get; set; }
+        public int CategoryId { get; set; }
+        public double Amount { get; set; }
+        public string Reason { get; set; }
+        public DateTime CreationTime { get; set; }
+
+        public virtual Category Category { get; set; }
+        public virtual Profile User { get; set; }
+    }
+}

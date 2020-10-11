15f3242cf0df2d04c88db86bdbef1cb6115eb2fa
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-11 21:45:29 +0300

    Delete Authenticate User View Model

--- QutebaApp-Data/ViewModels/AuthenticateUserVM.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace QutebaApp_Data.ViewModels
-{
-    public class AuthenticateUserVM
-    {
-        public string DisplayName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}

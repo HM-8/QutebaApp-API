665b1d0a86c7052e4392a9c51cfc9ca6d9baaf9d
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-12 18:58:20 +0300

    Add An Endpoint To Get Token Information

--- QutebaApp-API/Controllers/AuthController.cs
+++ QutebaApp-API/Controllers/AuthController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FirebaseAdmin.Auth;
+using Microsoft.AspNetCore.Mvc;
 using QutebaApp_Core.Services.Interfaces;
+using QutebaApp_Data.ViewModels;
+using System.Threading.Tasks;
 
 namespace QutebaApp_API.Controllers
 {
@@ -12,6 +15,25 @@ namespace QutebaApp_API.Controllers
         public AuthController(IAuthService authService)
         {
             this.authService = authService;
+        }
+
+        [HttpPost]
+        [Route("getTokenInfo")]
+        public async Task<ActionResult<GeneralUserVM>> GetTokenInfo([FromForm] string token)
+        {
+            FirebaseToken firebaseToken = await FirebaseAuth.DefaultInstance.VerifyIdTokenAsync(token);
+
+            UserRecord user = await FirebaseAuth.DefaultInstance.GetUserAsync(firebaseToken.Uid);
+
+            GeneralUserVM generalUserVM = new GeneralUserVM()
+            {
+                UID = user.Uid,
+                Name = user.DisplayName,
+                Email = user.Email,
+                Claims = user.CustomClaims
+            };
+
+            return new JsonResult(generalUserVM);
         }
     }
 }

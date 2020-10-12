86f437858d1e93a781f7a07653828f55e9d3d6f0
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-12 18:43:30 +0300

    Add Authenticated User View Model

---
+++ QutebaApp-Data/ViewModels/AuthenticatedUserVM.cs
@@ -0,0 +1,14 @@
+﻿using QutebaApp_Data.Models;
+using System.Collections.Generic;
+
+namespace QutebaApp_Data.ViewModels
+{
+    public class AuthenticatedUserVM
+    {
+        public string UID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Profile Profile { get; set; }
+        public IReadOnlyDictionary<string, object> Claims { get; set; }
+    }
+}

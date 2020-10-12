74187fbef8d24373604f02872f4f0ba8c4c117e7
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-12 18:43:00 +0300

    Add General User View Model

---
+++ QutebaApp-Data/ViewModels/GeneralUserVM.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace QutebaApp_Data.ViewModels
+{
+    public class GeneralUserVM
+    {
+        public string UID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public IReadOnlyDictionary<string, object> Claims { get; set; }
+    }
+}

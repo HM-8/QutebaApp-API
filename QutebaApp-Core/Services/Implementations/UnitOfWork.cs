f1a604cf3326233d8cfd5a8f1af9a0e0f585b635
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-17 14:24:32 +0300

    Add UnitOfWork

---
+++ QutebaApp-Core/Services/Implementations/UnitOfWork.cs
@@ -0,0 +1,106 @@
+﻿using QutebaApp_Core.Services.Interfaces;
+using QutebaApp_Data.Data;
+using QutebaApp_Data.Models;
+using System;
+
+namespace QutebaApp_Core.Services.Implementations
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private bool isDisposed = false;
+        private QutebaAppDbContext context;
+        GenericRepository<Account> accountRepository;
+        GenericRepository<Profile> profileRepository;
+        GenericRepository<Role> roleRepository;
+        GenericRepository<Category> categoryRepository;
+        GenericRepository<Spending> spendingRepository;
+
+        public UnitOfWork(QutebaAppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public GenericRepository<Account> AccountRepository
+        {
+            get
+            {
+                if (this.accountRepository == null)
+                {
+                    this.accountRepository = new GenericRepository<Account>(context);
+                }
+                return accountRepository;
+            }
+        }
+
+        public GenericRepository<Profile> ProfileRepository
+        {
+            get
+            {
+                if (this.profileRepository == null)
+                {
+                    this.profileRepository = new GenericRepository<Profile>(context);
+                }
+                return profileRepository;
+            }
+        }
+
+        public GenericRepository<Role> RoleRepository
+        {
+            get
+            {
+                if (this.roleRepository == null)
+                {
+                    this.roleRepository = new GenericRepository<Role>(context);
+                }
+                return roleRepository;
+            }
+        }
+
+        public GenericRepository<Category> CategoryRepository
+        {
+            get
+            {
+                if (this.categoryRepository == null)
+                {
+                    this.categoryRepository = new GenericRepository<Category>(context);
+                }
+                return categoryRepository;
+            }
+        }
+
+        public GenericRepository<Spending> SpendingRepository
+        {
+            get
+            {
+                if (this.spendingRepository == null)
+                {
+                    this.spendingRepository = new GenericRepository<Spending>(context);
+                }
+                return spendingRepository;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.isDisposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.isDisposed = true;
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+    }
+}
---
+++ QutebaApp-Core/Services/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using QutebaApp_Core.Services.Implementations;
+using QutebaApp_Data.Models;
+using System;
+
+namespace QutebaApp_Core.Services.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        GenericRepository<Account> AccountRepository { get; }
+        GenericRepository<Profile> ProfileRepository { get; }
+        GenericRepository<Role> RoleRepository { get; }
+        GenericRepository<Category> CategoryRepository { get; }
+        GenericRepository<Spending> SpendingRepository { get; }
+
+        void Save();
+    }
+}

a413d6867ac13f9e8bb975e48df392271b06e46e
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-22 20:18:34 +0300

    Update IAuthService

--- QutebaApp-Core/Services/Interfaces/IAuthService.cs
+++ QutebaApp-Core/Services/Interfaces/IAuthService.cs
@@ -1,12 +1,15 @@
 ﻿using QutebaApp_Data.ViewModels;
-using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Security.Claims;
 
 namespace QutebaApp_Core.Services.Interfaces
 {
     public interface IAuthService
     {
-        Task<GeneralUserVM> Register(string token, string role);
-        Task<GeneralUserVM> Login(string token);
-        Task SetCustomClaims(string uid, string role);
+        UserVM Register(AuthenticateUserVM authenticateUser, string role, string createdAccountWith);
+        UserVM Login(AuthenticateUserVM authenticateUser);
+        IEnumerable<Claim> SetCustomClaims(int id, string role);
+        AuthenticatedUserVM GetToken(UserVM user, IEnumerable<Claim> claims);
+        string Encrypt(string stringToEncrypt);
     }
 }

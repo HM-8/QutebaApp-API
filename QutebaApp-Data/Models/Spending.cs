de624bb8c7e1d8b5eaa4daadbb2fc22e1bc75982
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-22 20:10:32 +0300

    Update Spending Model

--- QutebaApp-Data/Models/Spending.cs
+++ QutebaApp-Data/Models/Spending.cs
@@ -1,15 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace QutebaApp_Data.Models
 {
     public partial class Spending
     {
         public int Id { get; set; }
-        public string UserUid { get; set; }
+        public int UserId { get; set; }
         public int CategoryId { get; set; }
         public double Amount { get; set; }
         public string Reason { get; set; }
-        public DateTime CreationTime { get; set; }
+        public DateTime SpendingCreationTime { get; set; }
 
         public virtual Category Category { get; set; }
         public virtual Profile User { get; set; }

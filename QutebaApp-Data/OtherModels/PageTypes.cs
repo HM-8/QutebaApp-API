63a59a349bfc17d2a670b54d4a114b9bd031fbd5
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-22 19:59:38 +0300

    Add Enum Class For Page Types

---
+++ QutebaApp-Data/OtherModels/PageTypes.cs
@@ -0,0 +1,12 @@
+﻿namespace QutebaApp_Data.OtherModels
+{
+    public enum PageTypes
+    {
+        RegisterSuperAdmin = 1,
+        RegisterAdmin = 2,
+        RegisterUser = 3,
+        LogInSuperAdmin = 4,
+        LogInAdmin = 5,
+        LogInUser = 6
+    }
+}

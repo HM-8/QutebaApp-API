8d58ceca2e88e5e7d4bdf27b8c520674f0911dec
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-22 20:08:22 +0300

    Update Account Model to User Model

---
+++ QutebaApp-Data/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QutebaApp_Data.Models
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Fullname { get; set; }
+        public int RoleId { get; set; }
+        public string CreatedAccountWith { get; set; }
+        public DateTime UserCreationTime { get; set; }
+
+        public virtual Role Role { get; set; }
+        public virtual Profile Profiles { get; set; }
+    }
+}

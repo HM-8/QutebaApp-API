03f77ca78d202ff20d3ae4cb91446046c8357de5
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-27 01:32:31 +0300

    Add CreateProfile View Model

---
+++ QutebaApp-Data/ViewModels/CreateProfileVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QutebaApp_Data.ViewModels
+{
+    public class CreateProfileVM
+    {
+        public string Username { get; set; }
+        public string PhotoUrl { get; set; }
+    }
+}

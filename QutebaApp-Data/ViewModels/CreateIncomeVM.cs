01c31656616d1c14319490771fad58bf2d6f663a
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-27 03:40:09 +0300

    Add CreateIncome View Model

---
+++ QutebaApp-Data/ViewModels/CreateIncomeVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QutebaApp_Data.ViewModels
+{
+    public class CreateIncomeVM
+    {
+        public int IncomeCategoryId { get; set; }
+        public double IncomeAmount { get; set; }
+    }
+}

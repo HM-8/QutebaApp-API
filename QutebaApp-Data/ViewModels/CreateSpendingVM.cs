cdcf85200dbcf81ec6c5d8f9d71eeb3a4bb61354
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-27 03:40:23 +0300

    Add CreateSpending View Model

---
+++ QutebaApp-Data/ViewModels/CreateSpendingVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QutebaApp_Data.ViewModels
+{
+    public class CreateSpendingVM
+    {
+        public int SpendingCategoryId { get; set; }
+        public double SpendingAmount { get; set; }
+        public string Reason { get; set; }
+    }
+}

35c9e748c398982c845b67961f9f0732ebb6ceae
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-29 01:37:31 +0300

    Add Code Model

---
+++ QutebaApp-Data/Models/Code.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QutebaApp_Data.Models
+{
+    public class Code
+    {
+        public int UserId { get; set; }
+        public int CodeDigit { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}

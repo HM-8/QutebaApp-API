b659faf179fde464335e962f7a5981dadc087df6
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-10-29 01:12:10 +0300

    Add Service To Email Password Reset Code

--- QutebaApp-Core/Services/Interfaces/IEmailSenderService.cs
+++ QutebaApp-Core/Services/Interfaces/IEmailSenderService.cs
@@ -1,7 +1,10 @@
-﻿namespace QutebaApp_Core.Services.Interfaces
+﻿using QutebaApp_Data.ViewModels;
+using System.Threading.Tasks;
+
+namespace QutebaApp_Core.Services.Interfaces
 {
     public interface IEmailSenderService
     {
-
+        Task<bool> SendForgotPasswordEmailAsync(DynamicTemplateDataVM dynamicTemplateData);
     }
 }

b958969241d6e2ca2c3aa996834cda01bbe96b07
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-11-01 02:38:19 +0300

    Add GetTotalBalance Endpoint

--- QutebaApp-API/Controllers/UserController.cs
+++ QutebaApp-API/Controllers/UserController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using QutebaApp_Core.Services.Interfaces;
+using System;
+using System.Linq;
 
 namespace QutebaApp_API.Controllers
 {
@@ -12,6 +15,29 @@ namespace QutebaApp_API.Controllers
         public UserController(IUnitOfWork unitOfWork)
         {
             this.unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        [Route("gettotalbalance")]
+        [Authorize(Roles = "user")]
+        public IActionResult GetTotalBalance()
+        {
+            var userId = Convert.ToInt32(HttpContext.User.FindFirst("userId").Value);
+
+            var incomes = unitOfWork.IncomeRepository.FindAllBy(i => i.UserId == userId);
+
+            if (incomes != null)
+            {
+                double total = 0;
+                foreach (var item in incomes)
+                {
+                    total += item.IncomeAmount;
+                }
+
+                return new JsonResult(total);
+            }
+
+            return new JsonResult("You have zero income");
         }
     }
 }

7fe2090792753b3ddf7b2c177f55580c422fcbb1
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-11-01 00:39:04 +0300

    Add User Controller

---
+++ QutebaApp-API/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QutebaApp_Core.Services.Interfaces;
+
+namespace QutebaApp_API.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public UserController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+    }
+}

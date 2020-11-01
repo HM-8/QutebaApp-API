3ea1d7b62fa52a02e83cbf80a13686e9984bb666
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-11-01 03:08:55 +0300

    Add IncomeDashboard View Model

---
+++ QutebaApp-Data/ViewModels/IncomeDashboardVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QutebaApp_Data.ViewModels
+{
+    public class IncomeDashboardVM
+    {
+        public string IncomeCategoryName { get; set; }
+        public DateTime IncomeCreationTime { get; set; }
+        public double IncomeAmount { get; set; }
+    }
+}

134075aacd07e38b7a3065bdd9e540a631bf212b
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-11-01 04:06:02 +0300

    Add SpendingDashboard View Model

---
+++ QutebaApp-Data/ViewModels/SpendingDashboardVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace QutebaApp_Data.ViewModels
+{
+    public class SpendingDashboardVM
+    {
+        public string SpendingCategoryName { get; set; }
+        public DateTime SpendingCreationTime { get; set; }
+        public double SpendingAmount { get; set; }
+    }
+}

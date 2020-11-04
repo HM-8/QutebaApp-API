dbb4e6779b278e2006cdb09e116eb433e67dca4a
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-11-05 01:36:47 +0300

    Add Update Spending Endpoint

--- QutebaApp-API/Controllers/SpendingController.cs
+++ QutebaApp-API/Controllers/SpendingController.cs
@@ -23,7 +23,7 @@ namespace QutebaApp_API.Controllers
         [HttpPost]
         [Route("addspending")]
         [Authorize(Roles = "user")]
-        public IActionResult AddIncome(CreateSpendingVM spendingVM)
+        public IActionResult AddSpending(CreateSpendingVM spendingVM)
         {
             var userId = Convert.ToInt32(HttpContext.User.FindFirst("userId").Value);
 
@@ -107,6 +107,48 @@ namespace QutebaApp_API.Controllers
             };
 
             return new JsonResult(dashboardCardVM);
+        }
+
+        [HttpPatch]
+        [Route("updatespending")]
+        [Authorize(Roles = "user")]
+        public IActionResult UpdateSpending(SpendingDashboardVM spendingDashboardVM)
+        {
+            var userId = Convert.ToInt32(HttpContext.User.FindFirst("userId").Value);
+
+            var spending = unitOfWork.SpendingRepository.FindBy(i => i.UserId == userId && i.Id == spendingDashboardVM.ID, "SpendingCategory");
+            
+            var category = unitOfWork.CategoryRepository.FindBy(c => c.CategoryName == spendingDashboardVM.SpendingCategoryName && c.CategoryType == "spending");
+            
+            if (spending != null && category != null && category.Id == spending.SpendingCategory.Id)
+            {
+                unitOfWork.SpendingRepository.DetachEntry(spending);
+                unitOfWork.CategoryRepository.DetachEntry(category);
+
+                Spending updateSpending = new Spending()
+                {
+                    Id = spending.Id,
+                    UserId = spending.UserId,
+                    SpendingCategoryId = category.Id,
+                    SpendingAmount = spendingDashboardVM.SpendingAmount,
+                    SpendingCreationTime = spending.SpendingCreationTime,
+                };
+
+                unitOfWork.SpendingRepository.Update(updateSpending);
+                unitOfWork.Save();
+
+                SpendingDashboardVM updatespendingDashboard = new SpendingDashboardVM()
+                {
+                    ID = updateSpending.Id,
+                    SpendingCategoryName = category.CategoryName,
+                    SpendingCreationTime = updateSpending.SpendingCreationTime,
+                    SpendingAmount = updateSpending.SpendingAmount
+                };
+
+                return new JsonResult(updatespendingDashboard);
+            }
+
+            return new JsonResult("Error");
         }
     }
 }

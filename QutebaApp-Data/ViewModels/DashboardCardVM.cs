34fd6d5471e1c923e95cd1c531d7b35647133407
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-11-04 16:10:50 +0300

    Add DashboardCard View Model

---
+++ QutebaApp-Data/ViewModels/DashboardCardVM.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace QutebaApp_Data.ViewModels
+{
+    public class DashboardCardVM
+    {
+        public DateTime RecentTransactionDate { get; set; }
+        public double Total { get; set; }
+    }
+}

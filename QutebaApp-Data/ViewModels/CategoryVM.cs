180f39666f7f7c3534910ba97840348fc68458f8
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-11-06 02:01:43 +0300

    Add Category View Model

---
+++ QutebaApp-Data/ViewModels/CategoryVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QutebaApp_Data.ViewModels
+{
+    public class CategoryVM
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryType { get; set; }
+        public DateTime CategoryCreationTime { get; set; }
+    }
+}

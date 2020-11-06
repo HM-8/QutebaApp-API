56edfa13c650d774d918076481b511c86af3664a
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-11-06 23:47:34 +0300

    Add ListDaily View Model

---
+++ QutebaApp-Data/ViewModels/ListDailyVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QutebaApp_Data.ViewModels
+{
+    public class ListDailyVM
+    {
+        public string Hour { get; set; }
+        public double Amount { get; set; }
+    }
+}
